c92a7c0bee8b22691d284255ebba6865e9dc27c6
David Cantú Delgado <david.cantu.delgado@tec.mx>
2021-07-31 19:24:15 -0500

    02Licencia

--- assignments/02Licencia/tests/input_data.py
+++ assignments/02Licencia/tests/input_data.py
@@ -2,27 +2,34 @@
 
 # List of lists, where each of the inner lists has all the inputs for a single test
 input_values = [
-    ["19", "n"],
-    ["20", "s"],
-    ["18", "r"],
-    ["0"],
-    ["12"]
-    ]
-
-# List of lists, where each inner list has all the output/prints that end in the terminal
-output_values = [
-    ["Ingresa tu edad: ","¿Tienes identificación oficial? (s/n): ", "No cumples requisitos" ],
-    ["Ingresa tu edad: ","¿Tienes identificación oficial? (s/n): ", "Trámite de licencia concedido"],
-    ["Ingresa tu edad: ","¿Tienes identificación oficial? (s/n): ", "Respuesta incorrecta"],
-    ["Ingresa tu edad: ", "Respuesta incorrecta"],
-    ["Ingresa tu edad: ", "No cumples requisitos"]
-    ]
-
-# List of hints for each test, in case of an error
-error_messages = [
-    ["Revisa que pasa si cumples edad, pero no con credencial"], 
-    ["Revisa qué debe pasar si cumples con los dos requisitos"],
-    ["Revisa qué debes pasar si tecleas de manera incorrecta la respuesta de s/n"],
-    ["Revisa qué debe pasar si te mandan un dato inválido para la edad"],
-    ["Revisa qué deb pasar si es un menor de edad"]
+    # Test case 1
+        (
+            ["19", "n"],
+            ["Ingresa tu edad: ","¿Tienes identificación oficial? (s/n): ", "No cumples requisitos" ],
+            ["Revisa que pasa si cumples edad, pero no con credencial"]
+        ),
+    # Test case 2
+        ( 
+            ["20", "s"],
+            ["Ingresa tu edad: ","¿Tienes identificación oficial? (s/n): ", "Trámite de licencia concedido"],
+            ["Revisa qué debe pasar si cumples con los dos requisitos"]
+        ),
+    # Test case 3
+        (
+            ["18", "r"],
+            ["Ingresa tu edad: ","¿Tienes identificación oficial? (s/n): ", "Respuesta incorrecta"],
+            ["Revisa qué debes pasar si tecleas de manera incorrecta la respuesta de s/n"]
+        ),
+    # Test case 4
+        (
+            ["0"],
+            ["Ingresa tu edad: ", "Respuesta incorrecta"],
+            ["Revisa qué debe pasar si te mandan un dato inválido para la edad"]
+        ),
+    # Test case 5
+        (
+            ["12"],
+            ["Ingresa tu edad: ", "No cumples requisitos"],
+            ["Revisa qué deb pasar si es un menor de edad"]
+        )
     ]
