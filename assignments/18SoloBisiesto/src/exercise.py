56427a40629a653f5dd378b68a8281825e0a6132
dcbalderas <dc.balderassilva@tec.mx>
2021-08-11 16:54:36 -0500

    Update exercise.py

--- assignments/18SoloBisiesto/src/exercise.py
+++ assignments/18SoloBisiesto/src/exercise.py
@@ -1,14 +1,7 @@
+
 def main():
-    #escribe tu código abajo de esta línea
+    # Escribe el código adecuado para completar el programa
     year = int(input("Año: "))
-    if year > 0:
-        if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-            print(True)
-        else:
-            print(False)
-    else:
-        print("Dato incorrecto")
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
