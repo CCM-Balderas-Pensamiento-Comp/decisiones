5293fc732e0a75d3b395b440b14beaa63c20611b
dcbalderas <dc.balderassilva@tec.mx>
2021-08-11 16:52:52 -0500

    Update exercise.py

--- assignments/14LargestOfThree/src/exercise.py
+++ assignments/14LargestOfThree/src/exercise.py
@@ -7,7 +7,7 @@ def main():
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
     c = int(input("Enter third number: "))
-    print(largest_of_three(a, b, c))
+    print(check_grade(a, b, c))
 
 
 if __name__ == '__main__':
