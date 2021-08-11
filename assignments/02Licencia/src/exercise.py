e249d18202bfae0b3103172fefe93f04ba8d9f29
dcbalderas <dc.balderassilva@tec.mx>
2021-08-11 16:46:59 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,19 +1,9 @@
-
 def main():
     edad = int(input("Ingresa tu edad: "))
-    if edad > 0:
-        if edad >= 18:
-            credencial=input("¿Tienes identificación oficial? (s/n): ")
-            if credencial=="s" or credencial=="S":
-                print("Trámite de licencia concedido")
-            elif credencial=="n" or credencial=="N":
-                print("No cumples requisitos")
-            else:
-                print("Respuesta incorrecta")
-        else:
-            print("No cumples requisitos")
-    else:
-        print("Respuesta incorrecta")
+    # Escribe el código adecuado para completar el programa
+    # Para pedir el dato de la idetificación oficial emplea este mensaje:
+    # "¿Tienes identificación oficial? (s/n): "
+    
 
 
 if __name__ == '__main__':
